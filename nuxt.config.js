2201f4ad88da0e27917c69356742fd14ee49533c
Antoine Guglielmi <antoine.guglielmi@insign.fr>
2022-03-17 16:37:44 +0100

    fix plugin implementation

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/firebase.js'
+    '~/plugins/firebase.js',
+    '~/plugins/myPlugin.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
